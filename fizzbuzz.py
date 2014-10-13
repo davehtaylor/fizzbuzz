8331690fd0833ae76c9c95c2b0023d2505f4df1d
Dave Taylor <davehtaylor@me.com>
2014-10-13 14:16:08 -0400

    Added comments, changed range to xrange, and removed suplerflous variable and incrementer.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -4,19 +4,15 @@
 # is divisible by 5, 'Buzz' is printed instead. If the number is divisible by both 3 and 5, 'Fizzbuzz' is
 # printed instead.
 
-i = 1
+for i in xrange (1,101):            # Print out the numbers from 1 - 100.
+    if i%3 == 0 and i%5 == 0:       # Evaluate if the number is divisible by both 3 and 5. Need to do this 
+        print "Fizzbuzz"            # first, or this condition would never evaluate.
 
-for i in range (1,101):
-    if i%3 == 0 and i%5 == 0:
-        print "Fizzbuzz"
-
-    elif i%3 == 0:
+    elif i%3 == 0:                  # Evaluate if the number is divisible by 3.
         print "Fizz"
 
-    elif i%5 == 0:
+    elif i%5 == 0:                  # Evaluate if the number is diivisble by 5.
         print "Buzz"
 
-    else:
+    else:                           # Otherwise, just print out the number.
         print i
-
-    i += 1
