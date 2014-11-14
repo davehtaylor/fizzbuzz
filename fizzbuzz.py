b3440de254fff4df8211618ae71ab9e2c86a6b9b
Dave Taylor <davehtaylor@me.com>
2014-11-14 17:08:47 -0500

    Tidying up line lengths based on PEP8 style guide.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,18 +1,21 @@
 # Fizzbuzz
 #
-# Prints numbers from 1 to 100. If the number is divisible by 3, 'Fizz' is printed instead. If the number
-# is divisible by 5, 'Buzz' is printed instead. If the number is divisible by both 3 and 5, 'Fizzbuzz' is
-# printed instead.
+# Prints numbers from 1 to 100. If the number is divisible by 3, 'Fizz'
+# is printed instead. If the number is divisible by 5, 'Buzz' is 
+# printed instead. If the number is divisible by both 3 and 5, 
+# 'Fizzbuzz' is printed instead.
 
-for i in xrange (1,101):            # Print out the numbers from 1 - 100.
-    if i%3 == 0 and i%5 == 0:       # Evaluate if the number is divisible by both 3 and 5. Need to do this 
-        print "Fizzbuzz"            # first, or this condition would never evaluate.
+for i in xrange (1,101):       # Print out the numbers from 1 - 100.
 
-    elif i%3 == 0:                  # Evaluate if the number is divisible by 3.
+    if i%3 == 0 and i%5 == 0:  # Evaluate if the number is divisible by 
+        print "Fizzbuzz"       # both 3 and 5. Need to do this first, or 
+                               # this condition would never evaluate.
+
+    elif i%3 == 0:             # Evaluate if the number is divisible by 3
         print "Fizz"
 
-    elif i%5 == 0:                  # Evaluate if the number is diivisble by 5.
+    elif i%5 == 0:             # Evaluate if the number is diivisble by 5
         print "Buzz"
 
-    else:                           # Otherwise, just print out the number.
+    else:                      # Otherwise, just print out the number
         print i
